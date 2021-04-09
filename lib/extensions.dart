ceb40212d108a7ab28dc06381ce13c7ea4777ee4
lord <m_s_r@outlook.com>
2021-04-09 01:37:27 -0300

    trocando utils por extension methods

---
+++ lib/extensions.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+extension DoubleUtils on double {
+  double radianToDegree() => this * (180 / pi);
+}
+
+extension NumberUtils on num {
+  double normalize(double min, double max) {
+    final double result = (this - min) / (max - min);
+
+    if (result < 0.0) return 0.0;
+    if (result > 1.0) return 1.0;
+
+    return result;
+  }
+
+  double denormalize(double min, double max) {
+    final double result = (this - min) * (max - min);
+
+    if (result < min) return min;
+    if (result > max) return max;
+
+    return result;
+  }
+
+  double getPercentage(double percentage) => this * (percentage / 100);
+}
