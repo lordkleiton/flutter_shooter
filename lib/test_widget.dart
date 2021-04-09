51f0770610dd4871521a399be3c0cf5afa821f0a
lord <m_s_r@outlook.com>
2021-04-09 02:11:11 -0300

    melhorando o display das coordenadas

--- lib/test_widget.dart
+++ lib/test_widget.dart
@@ -61,56 +61,31 @@ class AppCanvas extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    drawGrid(canvas, size);
+    canvas.drawGrid(size);
 
-    //linha do ponteiro
     canvas.drawLine(start, end, Paint()..color = Colors.accents.first);
 
-    final paragraph = ParagraphBuilder(
-      ParagraphStyle(fontSize: 15),
-    )..addText(
-        '(x: ${display.dx.toStringAsFixed(4)}, y: ${display.dy.toStringAsFixed(4)})');
-
-    final blabla = paragraph.build();
-
-    final double paragraphWidth = 140;
-
-    blabla.layout(ParagraphConstraints(width: paragraphWidth));
-
-    canvas.drawParagraph(blabla,
-        Offset((size.width / 2) - (paragraphWidth / 2), size.height / 2));
+    drawCoordinates(canvas, size);
   }
 
   @override
   bool shouldRepaint(covariant AppCanvas oldDelegate) =>
       oldDelegate.end.dx != end.dx || oldDelegate.end.dy != end.dy;
 
-  void drawLine(
-      Canvas canvas, double xStart, double yStart, double xEnd, double yEnd) {
-    canvas.drawLine(Offset(xStart, yStart), Offset(xEnd, yEnd),
-        Paint()..color = Colors.accents.first);
-  }
+  void drawCoordinates(Canvas canvas, Size size) {
+    final double width = 140;
+    final String text =
+        '(x: ${display.dx.toStringAsFixed(4)}, y: ${display.dy.toStringAsFixed(4)})';
+    final ParagraphBuilder builder =
+        ParagraphBuilder(ParagraphStyle(fontSize: 15));
 
-  void drawGrid(Canvas canvas, Size size,
-      {int verticalLines = 4, int horizontalLines = 4}) {
-    drawVerticalGrid(canvas, size, verticalLines);
+    builder.addText(text);
 
-    drawHorizontalGrid(canvas, size, horizontalLines);
-  }
+    final Paragraph paragraph = builder.build();
 
-  void drawVerticalGrid(Canvas canvas, Size size, int qtyLines) {
-    final double horizontal = size.width / qtyLines;
+    paragraph.layout(ParagraphConstraints(width: width));
 
-    for (int i = 1; i < qtyLines; i++) {
-      drawLine(canvas, horizontal * i, 0, horizontal * i, size.height);
-    }
-  }
-
-  void drawHorizontalGrid(Canvas canvas, Size size, int qtyLines) {
-    final double vertical = size.height / qtyLines;
-
-    for (int i = 1; i < qtyLines; i++) {
-      drawLine(canvas, 0, vertical * i, size.width, vertical * i);
-    }
+    canvas.drawParagraph(
+        paragraph, Offset((size.width / 2) - (width / 2), size.height / 2));
   }
 }
