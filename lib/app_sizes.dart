87143e4b7dd0840985657bf1c5284881d2c4ba00
lord <m_s_r@outlook.com>
2021-04-08 22:48:49 -0300

    mais métodos úteis de respnsividade

--- lib/app_sizes.dart
+++ lib/app_sizes.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/widgets.dart';
+import 'utils.dart';
 
 class AppSizes {
   final BuildContext context;
+
   Size get size => MediaQuery.of(context).size;
   double get width => size.width;
   double get height => size.height;
+  double get midWidth => width / 2;
+  double get midHeight => height / 2;
 
   AppSizes({required this.context});
+
+  double responsiveWidth(double percentage) => getPercentage(width, percentage);
+
+  double responsiveHeight(double percentage) =>
+      getPercentage(height, percentage);
 }
